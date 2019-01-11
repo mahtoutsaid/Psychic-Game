fcdcbc33a43a06300e9aa4f1ff6627bbdef84b21
mahtoutsaid <mahtoutsaid0913@gmail.com>
2019-01-10 23:27:20 -0800

    01-10-19 changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,122 @@
+//var text;
+var possible = "abcdefghijklmnopqrstuvwxyz";
+var guessRemaining = 9; 
+      var wins = 1;
+      var lose = 1;
+      var arr = [];
+      
+    
+function letterGuess() {
+  
+   
+   text = possible.charAt(Math.floor(Math.random() * possible.length));
+   
+   console.log(text)
+   
+ 
+   
+     
+   document.onkeypress = compare;
+
+   
+        
+}
+
+function compare() {
+    
+   var k = event.key;
+   if(text==k){
+      document.getElementById("wins").innerHTML = wins++ 
+      document.getElementById("inpt").value = [];
+      document.getElementById("imag").innerHTML = (" ");
+      document.getElementById("letterToGuess").innerHTML = ("*");
+      guessRemaining = 9;
+      document.getElementById("par").innerHTML = guessRemaining;
+      arr = [];
+      document.getElementById("letterToGuess").innerHTML = ("Good Job!!!");
+      document.getElementById("imag").innerHTML = "<img src ='assets/images/happy.jpeg'>";
+      letterGuess();
+   } else {
+      // console.log("else")
+      for (var i=0; i < guessRemaining+1; i++) {
+         arr.push(k);
+         document.getElementById("inpt").value = arr;
+         guessRemaining -= 1;
+         document.getElementById("par").innerHTML = guessRemaining; 
+         // console.log(guessRemaining)
+         document.getElementById("imag").innerHTML = (" ")
+         document.getElementById("letterToGuess").innerHTML = ("*")
+
+         if (guessRemaining==0) {
+            document.getElementById("inpt").value = []; 
+            document.getElementById("loses").innerHTML = lose++
+            // document.getElementById("inpt").value = [];
+            document.getElementById("imag").innerHTML = "<img src =\"assets/images/sad.jpeg \">"
+            document.getElementById("letterToGuess").innerHTML = ("The letter to guess was : " + " ' " + text.toUpperCase() + " ' ");
+            guessRemaining=9;
+
+            document.getElementById("par").innerHTML = guessRemaining;
+            
+            arr = [];
+            
+            letterGuess();
+            
+         }
+         break;
+      }
+           
+    }
+    
+    
+ }
+ 
+document.onkeypress = letterGuess;
+
+/////////////////////////////////////////
+
+// function win()
+//     {
+//         if(text==k) 
+//             {
+   
+//                 arr.push(k);
+//                 document.getElementById("int").value = []; 
+//                 document.getElementById("wins").innerHTML = wins++
+//                 guessRemaining
+//                 letterGuess();
+//         //  console.log("you got it")
+//             }
+//     }
+    
+//     win();
+    
+
+//      if(text!=k)
+//       {
+        
+//         // console.log("you missed it")
+        
+//         for (var i=0; i < guessRemaining; i++)
+//         { 
+            
+//             document.getElementById("par").innerHTML = guessRemaining--; 
+//         // console.log(arr);
+        
+//            arr.push(k);
+//            document.getElementById("int").value = arr;
+     
+//              if (guessRemaining===0)
+//                  {
+//                     document.getElementById("int").value = []; 
+//                     document.getElementById("loses").innerHTML = lose++
+            
+            
+//                   }
+       
+//             break;
+       
+        
+//            }
+         
+    
+//        }
